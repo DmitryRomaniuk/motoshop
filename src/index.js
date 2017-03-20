cf07ddd19cf825b1518f5590724591832b3f5ac1
Dzmitry Ramaniuk2 <Dzmitry_Ramaniuk2@epam.com>
2017-03-20 15:22:27 +0300

    added react-router-dom, redux, immutable to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom'
+import {createStore, applyMiddleware} from 'redux';
+import reducer from './reducer';
+import {Provider} from 'react-redux';
 import App from './App';
 import './index.css';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducer);
+
 ReactDOM.render(
-  <App />,
+    <Provider store={store}>
+        <Router>
+            <Route exact path="/" component={App}/>
+        </Router>
+    </Provider>,
   document.getElementById('root')
 );
