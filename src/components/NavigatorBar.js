d79748750de6d8e5d5738a7f3214ec7f2c8786b8
Dzmitry Ramaniuk2 <Dzmitry_Ramaniuk2@epam.com>
2017-03-22 17:47:18 +0300

    added nav bar links

--- src/components/NavigatorBar.js
+++ src/components/NavigatorBar.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
+import {Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink} from 'reactstrap';
 import * as actionCreators from '../action_creators';
 import {connect} from 'react-redux';
 
-export const NavigatorBar =({isOpen, toggleNavBar}) => {
+export const NavigatorBar = ({isOpen, toggleNavBar}) => {
     return (
-        <div>
-            <Navbar color="faded" light toggleable>
-                <NavbarToggler right onClick={toggleNavBar}/>
-                <NavbarBrand href="/">reactstrap</NavbarBrand>
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/components/">Components</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
+        <Navbar color="faded" light toggleable>
+            <NavbarToggler right onClick={toggleNavBar}/>
+            <a className="header-logo__brand" rel="honda logo">Honda</a>
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/sport/">Супер Спорт</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/sport-touring/">Спорт Туризм</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/touring/">Туризм</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/naked/">Нейкиды</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/enduro/">Эндуро</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/crosser/">Кроссовые</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/quadracycle/">Квадроциклы</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/login/">
+                            <span className="header-login__svg" rel="login"/>
+                            <span>Log in</span>
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
     );
 };
 
