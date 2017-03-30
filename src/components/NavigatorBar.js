949230410c0e32adc6e4baaa279a3a2e345ec694
Dzmitry Ramaniuk2 <Dzmitry_Ramaniuk2@epam.com>
2017-03-30 18:03:54 +0300

    added change decrease react-router version

--- src/components/NavigatorBar.js
+++ src/components/NavigatorBar.js
@@ -1,47 +1,94 @@
 import React from 'react';
-import {Collapse, Navbar, NavbarToggler, Nav, NavItem, NavLink} from 'reactstrap';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 import * as actionCreators from '../action_creators';
 import {connect} from 'react-redux';
 
-export const NavigatorBar = ({isOpen, toggleNavBar}) => {
-    return (
-        <Navbar color="faded" light toggleable>
-            <NavbarToggler right onClick={toggleNavBar}/>
-                <NavLink href="/" className="header-logo__brand" rel="honda logo">Honda</NavLink>
-            <Collapse isOpen={isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                    <NavItem>
-                        <NavLink href="/super-sport/">Супер Спорт</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/sport-touring/">Спорт Туризм</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/touring/">Туризм</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/naked/">Нейкиды</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/enduro/">Эндуро</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/crosser/">Кроссовые</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/quadracycle/">Квадроциклы</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/login/">
-                            <span className="header-login__svg" rel="login"/>
-                            <span>Log in</span>
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
-    );
+const NavigatorBar = () => {
+    return (<Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="/" className="header-logo__brand" rel="honda logo">Honda</a>
+            </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+            <LinkContainer to="/super-sport/">
+                <NavItem eventKey={1}>Супер Спорт</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/sport-touring/">
+            <NavItem eventKey={2} >Спорт Туризм</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/touring/">
+            <NavItem eventKey={3} >Туризм</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/naked/">
+            <NavItem eventKey={4} >Нейкиды</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/enduro/">
+            <NavItem eventKey={5} >Эндуро</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/crosser/">
+            <NavItem eventKey={6} >Кроссовые</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/quadracycle/">
+            <NavItem eventKey={7} >Квадроциклы</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login/">
+            <NavItem eventKey={8} >
+            <span className="header-login__svg" rel="login"/>
+            <span>Log in</span>
+            </NavItem>
+            </LinkContainer>
+            {/*<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">*/}
+            {/*<MenuItem eventKey={3.1}>Action</MenuItem>*/}
+            {/*<MenuItem eventKey={3.2}>Another action</MenuItem>*/}
+            {/*<MenuItem eventKey={3.3}>Something else here</MenuItem>*/}
+            {/*<MenuItem divider />*/}
+            {/*<MenuItem eventKey={3.4}>Separated link</MenuItem>*/}
+            {/*</NavDropdown>*/}
+        </Nav>
+    </Navbar>)
 };
+
+// export const NavigatorBar = ({isOpen, toggleNavBar}) => {
+//     return (
+//         <Navbar color="faded" light toggleable>
+//             <NavbarToggler right onClick={toggleNavBar}/>
+//                 <NavLink href="/" className="header-logo__brand" rel="honda logo">Honda</NavLink>
+//             <Collapse isOpen={isOpen} navbar>
+//                 <Nav className="ml-auto" navbar>
+//                     <NavItem>
+//                         <NavLink href="/super-sport/">Супер Спорт</NavLink>
+//                     </NavItem>
+//                     <NavItem>
+//                         <NavLink href="/sport-touring/">Спорт Туризм</NavLink>
+//                     </NavItem>
+//                     <NavItem>
+//                         <NavLink href="/touring/">Туризм</NavLink>
+//                     </NavItem>
+//                     <NavItem>
+//                         <NavLink href="/naked/">Нейкиды</NavLink>
+//                     </NavItem>
+//                     <NavItem>
+//                         <NavLink href="/enduro/">Эндуро</NavLink>
+//                     </NavItem>
+//                     <NavItem>
+//                         <NavLink href="/crosser/">Кроссовые</NavLink>
+//                     </NavItem>
+//                     <NavItem>
+//                         <NavLink href="/quadracycle/">Квадроциклы</NavLink>
+//                     </NavItem>
+//                     <NavItem>
+//                         <NavLink href="/login/">
+//                             <span className="header-login__svg" rel="login"/>
+//                             <span>Log in</span>
+//                         </NavLink>
+//                     </NavItem>
+//                 </Nav>
+//             </Collapse>
+//         </Navbar>
+//     );
+// };
 
 function mapStateToProps(state) {
     return {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Route
-} from 'react-router-dom'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import {createStore} from 'redux';
 import reducer from './reducer';
 import {Provider} from 'react-redux';
 import App from './components/App';
 import PreviewMoto from './components/PreviewMoto';
-import {NavigatorBarContainer as NavigatorBar} from './components/NavigatorBar';
-import Footer from './components/Footer';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -18,21 +13,23 @@ const store = createStore(reducer);
 
 console.log(store);
 
+const listMoto=[{"name": "CBR1000RA Fireblade 2017", "srcImg":"/images/black_310.png", "price": 10000},
+{"name": "CBR300RA", "srcImg":"/images/CBR300RA_overview_new-1_00.png", "price": 12000},
+{"name": "CBR600RA 2017", "srcImg":"/images/cbr600_310.jpg", "price": 14000}];
+
+const component = (
+    <Router history={hashHistory}>
+        <Route path="/">
+            <IndexRoute component={App}/>
+            <Route exact path="super-sport" component={() => (<PreviewMoto listMoto={listMoto}/>)}/>
+        </Route>
+    </Router>
+);
+
+
 ReactDOM.render(
     <Provider store={store}>
-        <div>
-            <NavigatorBar />
-            <Router>
-                <div>
-                    <Route exact path="/" component={App}/>
-                    <Route exact path="/super-sport" component={() => (<PreviewMoto
-                        listMoto={[{"name": "CBR1000RA Fireblade 2017", "srcImg":"/images/black_310.png", "price": 10000},
-                            {"name": "CBR300RA", "srcImg":"/images/CBR300RA_overview_new-1_00.png", "price": 12000},
-                            {"name": "CBR600RA 2017", "srcImg":"/images/cbr600_310.jpg", "price": 14000}]} />)}/>
-                </div>
-            </Router>
-            <Footer />
-        </div>
+            {component}
     </Provider>,
     document.getElementById('root')
 );
