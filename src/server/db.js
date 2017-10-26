b8b99becffad4f40aef4efd756e5e42ab8521fe9
Dzmitry Ramaniuk <kanumowa@gmail.com>
2017-10-27 00:17:05 +0300

    added connection to postgreSQL

--- src/server/db.js
+++ src/server/db.js
@@ -1,49 +1,35 @@
 // @flow
+const { Client } = require('pg');
 
-// import mongoose from 'mongoose';
+const connectionString = process.env.DATABASE_URL;
+const parseURI = connectionString.match(/(\w+):([\w\d]+)@([\w\d.-]+):(\d+)\/([\d\w]+)/);
+const client = new Client({
+    user: parseURI[1],
+    password: parseURI[2],
+    database: parseURI[5],
+    port: parseURI[4],
+    host: parseURI[3],
+    ssl: true,
+});
+client.connect();
 
-// mongoose.Promise = global.Promise;
+client.query('SELECT NOW()', (err, res) => {
+    // eslint-disable-next-line no-console
+    console.log(err, res);
+    client.end();
+});
 
-// mongoose.connect(process.env.MONGODB_URI);
-
-// /* eslint-disable no-console */
-// mongoose.connection.on('connected', () => console.log('DB connected.'));
-// mongoose.connection.on('error', err => console.error(err));
-// mongoose.connection.on('disconnected', () => console.log('DB disconnected.'));
-// /* eslint-enable no-console */
-
-// process.on('SIGINT', () => {
-//   mongoose.connection.close(() => {
-//     process.exit();
-//   });
-// });
-
-// /* eslint-disable import/no-unresolved */
-// import * as admin from 'firebase-admin';
-// import serviceAccount from '../../motoshop-firebase-access.json';
-// /* eslint-disable import/no-unresolved */
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: 'https://motoshop-632ec.firebaseio.com',
-// });
-/* eslint-disable no-console */
 const admin = require('firebase-admin');
-
-const serviceAccount = require('../../motoshop-firebase-access.json');
+const serviceAccount = require('./motoshop-firebase-access.json');
 
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://motoshop-632ec.firebaseio.com',
-  // databaseAuthVariableOverride: {
-  //   uid: 'Xep3lILIfkMuaVoUwDAD4PjYSYl1',
-  // },
 });
 
 export const typesMotocycles = () => new Promise((resolve, reject) => {
     admin.database().ref('typesMoto').on('value', (snapshot) => {
-    // console.log(snapshot.val());
         resolve(snapshot.val());
     }, (errorObject) => {
         console.log('The read failed: ' + errorObject.code);
@@ -51,10 +37,8 @@ export const typesMotocycles = () => new Promise((resolve, reject) => {
     });
 });
 
-// const getUsers = db.ref('users');
 export const users = () => new Promise((resolve, reject) => {
     admin.database().ref('users').on('value', (snapshot) => {
-    // console.log(snapshot.val());
         resolve(snapshot.val());
     }, (errorObject) => {
         console.log('The read failed: ' + errorObject.code);
@@ -64,7 +48,6 @@ export const users = () => new Promise((resolve, reject) => {
 
 export const homeList = () => new Promise((resolve, reject) => {
     admin.database().ref('overview').on('value', (snapshot) => {
-    // console.log(snapshot.val());
         resolve(snapshot.val());
     }, (errorObject) => {
         console.log('The read failed: ' + errorObject.code);
@@ -74,7 +57,6 @@ export const homeList = () => new Promise((resolve, reject) => {
 
 export const homeRoutes = () => new Promise((resolve, reject) => {
     admin.database().ref('typesMoto').on('value', (snapshot) => {
-    // console.log(snapshot.val());
         resolve(snapshot.val());
     }, (errorObject) => {
         console.log('The read failed: ' + errorObject.code);
